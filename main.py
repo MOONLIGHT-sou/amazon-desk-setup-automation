3be27033a035599b7dd187fed922c09ce365c628
MOONLIGHT-sou <thecompliancedash@gmail.com>
2026-08-18 01:56:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 print("Amazon Desk Setup Automation")
 print("System is starting...")
+import csv
+
+print("Amazon Desk Setup Automation Started!")
+
+with open("products.csv", "r", encoding="utf-8") as file:
+    products = csv.DictReader(file)
+
+    for product in products:
+        print("--------------------------------")
+        print("Product ID:", product["product_id"])
+        print("Product:", product["product_name"])
+        print("Category:", product["category"])
+        print("Amazon Link:", product["amazon_link"])
+        print("Used:", product["used"])
+
+print("--------------------------------")
+print("Products loaded successfully!")
